493305539a3a7861e4440b798a3c924254bfd9c3
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-27 02:10:09 +0900

    add d3-wordcloud

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import WordCloud from "react-d3-cloud";
+
 function App() {
-  return <div className="">hello</div>;
+  const data = [
+    { text: "Hey", value: 1000 },
+    { text: "lol", value: 200 },
+    { text: "first impression", value: 800 },
+    { text: "very cool", value: 1000000 },
+    { text: "duck", value: 10 },
+  ];
+
+  const fontSizeMapper = (word) => Math.log2(word.value) * 3;
+  const rotate = (word) => word.value % 360;
+
+  return (
+    <div>
+      <h2>hello</h2>
+      <WordCloud
+        data={data}
+        fontSizeMapper={fontSizeMapper}
+        width={400}
+        height={400}
+        /*rotate={rotate}*/
+      />
+    </div>
+  );
 }
 
 export default App;
