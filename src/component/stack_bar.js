45d55d63f1a01d5a603c07987a9a58940c9645bf
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-04-03 01:46:16 +0900

    stacked bar

--- src/component/stack_bar.js
+++ src/component/stack_bar.js
@@ -11,109 +11,117 @@ function stackedBar() {
   };
 
   const options = {
+    height: 100,
     animationEnabled: true,
     theme: "light2",
     title: {
-      text: "Evening Sales in a Restaurant",
+      text: "",
     },
     axisX: {
-      valueFormatString: "DDD",
+      /*valueFormatString: "",*/
     },
     axisY: {
-      prefix: "$",
+      tickLength: 0,
+
+      /*prefix: "",*/
     },
     toolTip: {
       shared: true,
     },
-    legend: {
+    /*legend: {
       cursor: "pointer",
       itemclick: toggleDataSeries,
-    },
+    },*/
     data: [
       {
         type: "stackedBar",
         name: "Meals",
-        showInLegend: "true",
+        //showInLegend: "true",
         xValueFormatString: "DD, MMM",
         yValueFormatString: "$#,##0",
-        dataPoints: [
-          { x: new Date(2018, 5, 25), y: 56 },
-          { x: new Date(2018, 5, 26), y: 45 },
-          { x: new Date(2018, 5, 27), y: 71 },
-          { x: new Date(2018, 5, 28), y: 41 },
-          { x: new Date(2018, 5, 29), y: 60 },
-          { x: new Date(2018, 5, 30), y: 75 },
-          { x: new Date(2018, 6, 1), y: 98 },
-        ],
+        dataPoints: [{ label: "tokyo", y: 56, x: 1 }],
       },
       {
         type: "stackedBar",
         name: "Snacks",
-        showInLegend: "true",
+        //showInLegend: "true",
         xValueFormatString: "DD, MMM",
         yValueFormatString: "$#,##0",
-        dataPoints: [
-          { x: new Date(2018, 5, 25), y: 86 },
-          { x: new Date(2018, 5, 26), y: 95 },
-          { x: new Date(2018, 5, 27), y: 71 },
-          { x: new Date(2018, 5, 28), y: 58 },
-          { x: new Date(2018, 5, 29), y: 60 },
-          { x: new Date(2018, 5, 30), y: 65 },
-          { x: new Date(2018, 6, 1), y: 89 },
-        ],
+        dataPoints: [{ label: "tokyo", y: 86, x: 1 }],
       },
       {
         type: "stackedBar",
         name: "Drinks",
-        showInLegend: "true",
+        //showInLegend: "true",
         xValueFormatString: "DD, MMM",
         yValueFormatString: "$#,##0",
-        dataPoints: [
-          { x: new Date(2018, 5, 25), y: 48 },
-          { x: new Date(2018, 5, 26), y: 45 },
-          { x: new Date(2018, 5, 27), y: 41 },
-          { x: new Date(2018, 5, 28), y: 55 },
-          { x: new Date(2018, 5, 29), y: 80 },
-          { x: new Date(2018, 5, 30), y: 85 },
-          { x: new Date(2018, 6, 1), y: 83 },
-        ],
+        dataPoints: [{ label: "tokyo", y: 48, x: 1 }],
       },
       {
         type: "stackedBar",
         name: "Dessert",
-        showInLegend: "true",
+        //showInLegend: "true",
         xValueFormatString: "DD, MMM",
         yValueFormatString: "$#,##0",
-        dataPoints: [
-          { x: new Date(2018, 5, 25), y: 61 },
-          { x: new Date(2018, 5, 26), y: 55 },
-          { x: new Date(2018, 5, 27), y: 61 },
-          { x: new Date(2018, 5, 28), y: 75 },
-          { x: new Date(2018, 5, 29), y: 80 },
-          { x: new Date(2018, 5, 30), y: 85 },
-          { x: new Date(2018, 6, 1), y: 105 },
-        ],
+        dataPoints: [{ label: "tokyo", y: 80, x: 1 }],
       },
       {
         type: "stackedBar",
         name: "Takeaway",
-        showInLegend: "true",
+        //showInLegend: "true",
         xValueFormatString: "DD, MMM",
         yValueFormatString: "$#,##0",
-        dataPoints: [
-          { x: new Date(2018, 5, 25), y: 52 },
-          { x: new Date(2018, 5, 26), y: 55 },
-          { x: new Date(2018, 5, 27), y: 20 },
-          { x: new Date(2018, 5, 28), y: 35 },
-          { x: new Date(2018, 5, 29), y: 30 },
-          { x: new Date(2018, 5, 30), y: 45 },
-          { x: new Date(2018, 6, 1), y: 25 },
-        ],
+        dataPoints: [{ label: "tokyo", y: 60, x: 1 }],
+      },
+
+      {
+        type: "stackedBar",
+        name: "a",
+        //showInLegend: "true",
+        xValueFormatString: "DD, MMM",
+        yValueFormatString: "$#,##0",
+        dataPoints: [{ label: "kanagawa", y: 56, x: 2 }],
+      },
+      {
+        type: "stackedBar",
+        name: "b",
+        //showInLegend: "true",
+        xValueFormatString: "DD, MMM",
+        yValueFormatString: "$#,##0",
+        dataPoints: [{ label: "kanagawa", y: 86, x: 2 }],
+      },
+      {
+        type: "stackedBar",
+        name: "c",
+        //showInLegend: "true",
+        xValueFormatString: "DD, MMM",
+        yValueFormatString: "$#,##0",
+        dataPoints: [{ label: "kanagawa", y: 48, x: 2 }],
+      },
+      {
+        type: "stackedBar",
+        name: "d",
+        //showInLegend: "true",
+        xValueFormatString: "DD, MMM",
+        yValueFormatString: "$#,##0",
+        dataPoints: [{ label: "kanagawa", y: 100, x: 2 }],
+      },
+      {
+        type: "stackedBar",
+        name: "e",
+        //showInLegend: "true",
+        xValueFormatString: "DD, MMM",
+        yValueFormatString: "$#,##0",
+        dataPoints: [{ label: "kanagawa", y: 60, x: 2 }],
       },
     ],
   };
+
   return (
     <div>
+      <CanvasJSChart
+        options={options} /*onRef={(ref) => (this.chart = ref)}*/
+      />
       <CanvasJSChart
         options={options} /*onRef={(ref) => (this.chart = ref)}*/
       />
